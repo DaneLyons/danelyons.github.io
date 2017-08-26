186ca813acd16d404ca33d1cbcdb31315ae13e20
Dane Lyons <duilen@gmail.com>
2017-08-26 15:06:16 -0700

    response json

--- traackr/soi.js
+++ traackr/soi.js
@@ -2,8 +2,9 @@ console.log('hello world!!!!');
 
 var json = {};
 
-fetch('soi.json', {
-  method: 'get'
-}).then(function(response) {
-  var json = response.body;
+fetch('soi.json').then(function(response) {
+  var j = response.json();
+  console.log('json', j);
+  json = j
+  return j;
 });
